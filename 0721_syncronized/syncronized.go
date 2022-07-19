6bba1bc7a17ce5cba798c5d6562405547a6b7fba
伯沣 <bofeng.lt@alibaba-inc.com>
2022-07-19 21:17:12 +0800

    syncronized with the percent of 40

--- 0721_syncronized/syncronized.go
+++ 0721_syncronized/syncronized.go
@@ -1,3 +1,86 @@
 // @Description: the tech of synchronization components in golang - sync.WaitGroup、mutex etc;
+// 探讨点:
+//      1. 为什么需要并发-数据竞争?
+//      2. 如何解决并发?
+//         - sync/atomic
+//         - sync.Mutex - 类 Java Lock 接口
+//         - sync.RMutex - 类 Java ReentrantReadWriteLock
+//      3. 多 routine 同步 - sync.WaitGroup
+//      4. 多 routine 情况下 -  sync.Once 只执行一次
+//      5. sync.Cond - 类Java Condition, wait/notify 经典范式=加锁+循环+等待
+//      6. sync.Map - 线程安全的 Map 容器;
+//      录制计划 - 分 6 小节
 
 package _721_syncronized
+
+import (
+	"sync"
+	"sync/atomic"
+)
+
+// add 单 routine 执行+操作.
+func add(w *sync.WaitGroup, num *int) {
+	defer w.Done()
+	*num = *num + 1
+}
+
+// UnsafeAdd 多 go routines 不安全相加.
+// 演示  data competition 即 数据竞争.
+func UnsafeAdd() {
+	var n int = 0
+	var wg *sync.WaitGroup = new(sync.WaitGroup)
+	wg.Add(1000)
+	for i := 0; i < 1000; i = i + 1 {
+		go add(wg, &n)
+	} // spawn 1000 new goroutines
+	wg.Wait()
+	println(n)
+}
+
+//SafeAddWithAtomic 使用 atomic.AddInt32() 进行原子性加, 类Java CAS.
+func SafeAddWithAtomic() {
+	var n int32 = 0
+	var wg *sync.WaitGroup = new(sync.WaitGroup)
+	wg.Add(1000)
+	for i := 0; i < 1000; i = i + 1 {
+		go func(*sync.WaitGroup, *int32) {
+			defer wg.Done()
+			atomic.AddInt32(&n, 1)
+		}(wg, &n)
+	} // spawn 1000 new goroutines
+	wg.Wait()
+	println(n)
+}
+
+// SafeAddAndMinusWithSyncMutex 多 go routines 同时 加、减操作通过使用 SyncMutex 实现 多routines 安全.
+func SafeAddAndMinusWithSyncMutex() {
+	var num int = 0
+
+	var mutex = new(sync.Mutex)
+	var wg = new(sync.WaitGroup)
+	wg.Add(2)
+
+	// Add
+	func(*sync.Mutex, *sync.WaitGroup) {
+		defer wg.Done()
+		for i := 0; i < 100000; i = i + 1 {
+			mutex.Lock()
+			num = num + 1
+			mutex.Unlock()
+		}
+	}(mutex, wg)
+
+	// Minus
+	go func(*sync.Mutex, *sync.WaitGroup) {
+		defer wg.Done()
+		for i := 0; i < 100000; i = i + 1 {
+			// 跟Java AbstractQueuedSynchronizer - 抽象队列同步器原理相同;
+			mutex.Lock()
+			num = num - 1
+			mutex.Unlock()
+		}
+	}(mutex, wg)
+
+	wg.Wait()
+	print(num)
+}
--- 0721_syncronized/syncronized_test.go
+++ 0721_syncronized/syncronized_test.go
@@ -1,3 +1,17 @@
 // @Description: tech golang sync、mutex 等同步源语功能;
 
 package _721_syncronized
+
+import "testing"
+
+func Test_UnsafeAdd(t *testing.T) {
+	UnsafeAdd()
+}
+
+func Test_SafeAddWithAtomic(t *testing.T) {
+	SafeAddWithAtomic()
+}
+
+func Test_SafeAddAndMinusWithSyncMutex(t *testing.T) {
+	SafeAddAndMinusWithSyncMutex()
+}
